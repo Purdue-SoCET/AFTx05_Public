60678f809b9826bec88fc772250a2790a853d8c6
Project30 SOC <socet30@ecegrid-thin4.ecn.purdue.edu>
2020-08-03 14:03:09 -0400

    made setup.tcl for pnr

--- design_flow_scripts/pnr/pnr.tcl
+++ design_flow_scripts/pnr/pnr.tcl
@@ -3,13 +3,8 @@
 ###########################################
 
 
-set PDK_PATH    ../../../MITLL_90_Dec2019/MITLL_PDK
-set STDLIB_PATH ../../../MITLL_90_Dec2019/MITLL90_STDLIB_8T/2019.12.20
+source setup.tcl
 
-set designName    top_chip
-set init_top_cell top_chip
-set init_verilog  prePnR_netlist/top_chip.v
-set init_io_file  ./scripts/top_chip.io
 setMultiCpuUsage -localCpu 8
 
 ################################################################
---
+++ design_flow_scripts/pnr/setup.tcl
@@ -0,0 +1,8 @@
+set PDK_PATH    ../../../MITLL_90_Dec2019/MITLL_PDK
+set STDLIB_PATH ../../../MITLL_90_Dec2019/MITLL90_STDLIB_8T/2019.12.20
+set IOLIB_PATH ../../../MITLL_90_Dec2019/MITLL90_IOPads_5_1_1
+
+set designName    top_chip
+set init_top_cell top_chip
+set init_verilog  prePnR_netlist/top_chip.v
+set init_io_file  ./scripts/top_chip.io
